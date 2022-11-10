61fe864231d7c9853c722cba8a4851f0642a7b34
MateusX10 <mateus11medeirosss@gmail.com>
2022-11-10 18:12:14 -0300

    adicionei funcionalidades

--- app.py
+++ app.py
@@ -8,9 +8,27 @@ app = Flask(__name__)
 def homepage():
     return render_template("index.html")
 
-@app.route("/contato")
+@app.route("/contato_aluno")
 def funil():
     return render_template("contato_aluno.html")        
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/apps integrados")
+def apps_integrados():
+    return render_template("apps_integrados.html")
+
+@app.route("/entrar")
+def entrar():
+    return render_template("entrar.html")
+
+@app.route("/cadastrar")
+def cadastrar():
+    return render_template("cadastrar.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
